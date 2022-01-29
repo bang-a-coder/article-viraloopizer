6b58f2b5930bfac0acb1a45857cf91f3a24f4011
bang-a-coder <nikitas4717@gmail.com>
2022-01-29 22:13:17 +0100

    init

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+	mode: "development",
+	entry: './src/index.js',
+	
+	devServer: {
+		contentBase: './dist'
+	},
+	devServer: {
+		static: './dist',
+  	},
+	module: {
+	      rules: [
+		{
+		      test: /\.scss$/,
+		      use: [
+		      // Creates `style` nodes from JS strings
+		      "style-loader",
+		      // Translates CSS into CommonJS
+		      "css-loader",
+		      // Compiles Sass to CSS
+		      "sass-loader",
+		      ],
+		},
+	      ],
+	    },
+      };
